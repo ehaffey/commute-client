2070a5fd7b8d83554e293f1056898a59bf5f6d98
ehaffey <e-haffey@hotmail.com>
2019-09-06 16:25:36 -0400

    Build auth front-end
    --Built barebones authentication front-end

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,78 @@
+'use strict'
+const store = require('./../store')
+
+const signUpSuccess = function (data) {
+  store.user = data.user
+  $('#message').text('Registered and signed in successfully')
+  $('#message').attr('class', 'success')
+  $('#auth').css('display', 'initial')
+  $('#pre-auth').css('display', 'none')
+  $('.text-field').val('')
+  //  $('#sign-up').reset()
+  // console.log('signUpSuccess ran')
+}
+
+const signUpFailure = function () {
+  $('#message').text('Sign up failed')
+  $('#message').attr('class', 'failure')
+  $('.text-field').val('')
+  // console.error('signUpFailure ran')
+}
+
+const signInSuccess = function (data) {
+  store.user = data.user
+  $('#message').text('Signed in successfully')
+  $('#message').attr('class', 'success')
+  $('#auth').css('display', 'initial')
+  $('#pre-auth').css('display', 'none')
+  $('.text-field').val('')
+  //  $('#sign-up').reset()
+  // console.log('signInSuccess ran')
+}
+
+const signInFailure = function () {
+  $('#message').text('Sign in failed')
+  $('#message').attr('class', 'failure')
+  $('.text-field').val('')
+  // console.error('signInFailure ran')
+}
+
+const changePasswordSuccess = function (data) {
+  $('#message').text('Password updated')
+  $('#message').attr('class', 'success')
+  $('.text-field').val('')
+  // console.log('changePaswordSuccess ran')
+}
+
+const changePasswordFailure = function () {
+  $('#message').text('Password couldn\'t be updated. Please try again.')
+  $('#message').attr('class', 'failure')
+  $('.text-field').val('')
+  // console.error('changePasswordFailure ran')
+}
+
+const signOutSuccess = function (data) {
+  clearTimeout(setTimeout)
+  $('#message').text('Sign off complete')
+  $('#message').attr('class', 'success')
+  $('#auth').css('display', 'none')
+  $('#pre-auth').css('display', 'initial')
+  // console.log('signOutSuccess ran')
+}
+
+const signOutFailure = function () {
+  $('#message').text('Sorry, you couldn\'t be signed off')
+  $('#message').attr('class', 'failure')
+  // console.error('signOutFailure ran')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
